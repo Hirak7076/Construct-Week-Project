95caed444b900beac350c9aad52fa117091be93b
Hirak7076 <hirakgoswami7076@gmail.com>
2024-07-22 03:03:14 +0530

    Update RegisterINFO.js

--- cypress/support/PageOBJECT/RegisterINFO.js
+++ cypress/support/PageOBJECT/RegisterINFO.js
@@ -9,7 +9,7 @@ class RegisterINFO{
         cy.xpath('//a[@href="https://tutorialsninja.com/demo/index.php?route=account/register"]').click();
         cy.xpath('//input[@name="firstname"]').clear().type('Hirak');
         cy.xpath('//input[@name="lastname"]').clear().type('Goswami');
-        cy.xpath('//input[@id="input-email"]').clear().type('Hirakgosw89101@gmail.com');
+        cy.xpath('//input[@id="input-email"]').clear().type('Hirakgoswaaaaaa89101@gmail.com');
         cy.xpath('//input[@name="telephone"]').clear().type('1234567890');
         cy.xpath('//input[@id="input-password"]').clear().type('1234');
         cy.xpath('//input[@id="input-confirm"]').clear().type('1234');
